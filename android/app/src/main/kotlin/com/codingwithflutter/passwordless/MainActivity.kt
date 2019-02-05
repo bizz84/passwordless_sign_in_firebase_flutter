893212278e28d61c0b87a21ac5258d36e6304a2b
bizz84 <bizz84@gmail.com>
2019-02-05 09:51:30 +0000

    Register EventChannel for processing incoming links

--- android/app/src/main/kotlin/com/codingwithflutter/passwordless/MainActivity.kt
+++ android/app/src/main/kotlin/com/codingwithflutter/passwordless/MainActivity.kt
@@ -3,11 +3,34 @@ package com.codingwithflutter.passwordless
 import android.os.Bundle
 
 import io.flutter.app.FlutterActivity
+import io.flutter.plugin.common.EventChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+  var linkStreamHandler: LinkStreamHandler? = null
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
+    linkStreamHandler = LinkStreamHandler()
+    val channel = EventChannel(flutterView, "linkHandler")
+    channel.setStreamHandler(linkStreamHandler)
+  }
+}
+
+class LinkStreamHandler: EventChannel.StreamHandler {
+
+  private var eventSink: EventChannel.EventSink? = null
+
+  override fun onListen(
+          arguments: Any?, eventSink: EventChannel.EventSink?) {
+    this.eventSink = eventSink
+  }
+  override fun onCancel(arguments: Any?) {
+    eventSink = null
+  }
+
+  fun handleLink(link: String) {
+    eventSink?.success(link)
   }
 }
