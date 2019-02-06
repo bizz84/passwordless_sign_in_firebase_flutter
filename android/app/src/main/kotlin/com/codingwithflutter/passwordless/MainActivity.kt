08a877418884be6726f8a42d784f27b208b3a453
bizz84 <bizz84@gmail.com>
2019-02-06 08:05:29 +0000

    Disable dynamic links code on emulator

--- android/app/src/main/kotlin/com/codingwithflutter/passwordless/MainActivity.kt
+++ android/app/src/main/kotlin/com/codingwithflutter/passwordless/MainActivity.kt
@@ -2,6 +2,7 @@ package com.codingwithflutter.passwordless
 
 import android.content.ContentValues.TAG
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import com.google.firebase.dynamiclinks.FirebaseDynamicLinks
@@ -24,20 +25,22 @@ class MainActivity: FlutterActivity() {
     // NOTE: This seems to crash the app on emulator with this log:
     // Error connecting to the service protocol: HttpException: Connection closed before full header was received, uri = http://127.0.0.1:53810/ws
     // See: https://github.com/flutter/flutter/issues/19056
-    // It appears to work fine on real device
-    FirebaseDynamicLinks.getInstance()
-            .getDynamicLink(intent)
-            .addOnSuccessListener(this) { pendingDynamicLinkData ->
-              // Get deep link from result (may be null if no link is found)
-              if (pendingDynamicLinkData != null) {
-                var deepLink = pendingDynamicLinkData.link
-                linkStreamHandler?.handleLink(deepLink.toString())
-                  Log.i(TAG, "getDynamicLink:onSuccess -> " + deepLink.toString())
-              } else {
-                  Log.i(TAG, "getDynamicLink:onSuccess -> no link found")
-              }
-            }
-            .addOnFailureListener(this) { e -> Log.w(TAG, "getDynamicLink:onFailure", e) }
+    // For this reason we only run this code on a real device (see https://stackoverflow.com/questions/2799097/how-can-i-detect-when-an-android-application-is-running-in-the-emulator)
+    if (!Build.FINGERPRINT.contains("generic")) {
+        FirebaseDynamicLinks.getInstance()
+                .getDynamicLink(intent)
+                .addOnSuccessListener(this) { pendingDynamicLinkData ->
+                    // Get deep link from result (may be null if no link is found)
+                    if (pendingDynamicLinkData != null) {
+                        var deepLink = pendingDynamicLinkData.link
+                        linkStreamHandler?.handleLink(deepLink.toString())
+                        Log.i(TAG, "getDynamicLink:onSuccess -> " + deepLink.toString())
+                    } else {
+                        Log.i(TAG, "getDynamicLink:onSuccess -> no link found")
+                    }
+                }
+                .addOnFailureListener(this) { e -> Log.w(TAG, "getDynamicLink:onFailure", e) }
+    }
   }
 }
 
